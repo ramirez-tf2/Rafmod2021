41b629033e51c318c6ff922def75d7a30d21c93d
ramirez <72025814+ramirez-tf2@users.noreply.github.com>
2021-12-17 16:58:13 -0500

    Wrong command line flags

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -5,6 +5,6 @@ PATHS="--hl2sdk-root=../build_scripts --mms-path=../build_scripts/mmsource-1.10 
 cd ..
 mkdir build
 cd build
-CC=gcc-9 CXX=g++-9 python3 ../configure.py $PATHS --sdks=tf2 --enable-debug --exclude-mods-debug --enable-optimize --exclude-mods-visualize
+CC=gcc-9 CXX=g++-9 python3 ../configure.py $PATHS --sdks=tf2 --enable-optimize --exclude-mods-debug --exclude-mods-visualize --exclude-vgui
 ambuild
 
